6f2a98ba482f7c8ac33a53f18713065153443135
Adityo-dev <computerscience01770@gmail.com>
2026-05-10 03:23:14 +0600

    Add config

--- server.ts
+++ server.ts
@@ -1,1 +1,11 @@
-console.log("Welcome To Edu Next Apis");
+import app from "./src/app.js";
+import { config } from "./src/config/config.js";
+
+const startServer = () => {
+  const port = config.port || 3000;
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+};
+
+startServer();
