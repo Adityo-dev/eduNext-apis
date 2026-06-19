cc215bb35256cadd5419664c2fa9bf8678a1b7ce
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2026-06-20 05:47:25 +0600

    feat: complete professional otp flow with token in verifyOtp

--- src/auth/authRoutes.ts
+++ src/auth/authRoutes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { login, signup } from "./authController.js";
+import { login, resendOtp, signup, verifyOtp } from "./authController.js";
 
 const authRouter = Router();
 
 // Public Auth API routes ->
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
+authRouter.post("/verify-otp", verifyOtp);
+authRouter.post("/resend-otp", resendOtp);
 
 export default authRouter;
