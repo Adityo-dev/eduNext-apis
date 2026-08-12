1a7f488e7b1e7ef368772951100b397030322016
Adityo-dev <computerscience01770@gmail.com>
2026-08-12 17:30:51 +0600

    fix: reorder imports and enhance student welcome route with authorization

--- src/overview/routes/overviewRoutes.ts
+++ src/overview/routes/overviewRoutes.ts
@@ -2,26 +2,32 @@ import { Router } from "express";
 import { authenticate, authorize } from "../../middlewares/auth.middleware.js";
 
 // Instructor Overview Controllers
-import { getInstructorWelcome } from "../instructor/getInstructorWelcome.controller.js";
 import { getInstructorOverviewStatus } from "../instructor/getInstructorOverviewStatus.controller.js";
+import { getInstructorWelcome } from "../instructor/getInstructorWelcome.controller.js";
 
 // Admin Overview Controllers
-import { getAdminWelcome } from "../admin/getAdminWelcome.controller.js";
 import { getAdminOverviewStats } from "../admin/getAdminOverviewStats.controller.js";
+import { getAdminWelcome } from "../admin/getAdminWelcome.controller.js";
 import { getQuickActionStats } from "../admin/getQuickActionStats.controller.js";
 
 // Student Overview Controllers
-import { getStudentWelcome } from "../student/getStudentWelcome.controller.js";
 import { getStudentCourseStats } from "../student/getStudentCourseStats.controller.js";
+import { getStudentWelcome } from "../student/getStudentWelcome.controller.js";
 
 const overviewRouter = Router();
 
 //  Student Overview Routes
-overviewRouter.get("/student/welcome", authenticate, getStudentWelcome);
+overviewRouter.get(
+  "/student/welcome",
+  authenticate,
+  authorize(["student"]),
+  getStudentWelcome,
+);
 
 overviewRouter.get(
   "/student/course-stats",
   authenticate,
+  authorize(["student"]),
   getStudentCourseStats,
 );
 
